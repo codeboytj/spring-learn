cbe53dfae533a650f4209dea316d90e465249597
tj <2238010512@qq.com>
2017-07-17 21:20:57 +0800

    单例模式的线程不安全

--- src/main/java/cumt/tj/learn/designpattern/singleton/README.md
+++ src/main/java/cumt/tj/learn/designpattern/singleton/README.md
@@ -1,3 +1,3 @@
 # 令人伤心的测试过程
 
-开了100个线程，并没有证明出来那个[SimpleSingleton](SimpleSingleton.java)是不安全的
+总算证明出来那个[SimpleSingleton](SimpleSingleton.java)是不安全的了
--- src/main/java/cumt/tj/learn/designpattern/singleton/SimpleSingleton.java
+++ src/main/java/cumt/tj/learn/designpattern/singleton/SimpleSingleton.java
@@ -7,36 +7,37 @@ package cumt.tj.learn.designpattern.singleton;
 public class SimpleSingleton{
 
     private static SimpleSingleton singleton;
-    public static int counter;
+    public static int counter=0;
 
     public SimpleSingleton() {
         counter++;
-    }
-
-    public static SimpleSingleton getInstance(){
-        if(singleton==null){
-            singleton=new SimpleSingleton();
-        }
-        return singleton;
-    }
-
-    public void showCounter(){
-//        counter++;
         if(counter!=1){
             System.out.println("线程不安全，对counter进行了2次自增，说明调用了2次构造函数");
         }
     }
 
+    public static SimpleSingleton getInstance() throws InterruptedException {
+        if(singleton==null){
+            Thread.sleep(30);
+            singleton=new SimpleSingleton();
+        }
+        return singleton;
+    }
+
     public static class RunClass implements Runnable{
         public void run() {
-            SimpleSingleton singleton=SimpleSingleton.getInstance();
-            singleton.showCounter();
+            try {
+                SimpleSingleton singleton=SimpleSingleton.getInstance();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+//            singleton.showCounter();
         }
     }
 
     public static void main(String[] args) {
         //创建1000个线程创建单例对象
-        for(int i=0;i<100;i++){
+        for(int i=0;i<1000;i++){
             new Thread(new RunClass()).start();
         }
         //按照道理说，应该会出现counter!=2的情况，但是却没有出现，简直无情
